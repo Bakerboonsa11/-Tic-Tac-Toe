fad4134cc1c844991fd4d59b1d56fdbc800a9de2
bonsa <bonsa>
2024-02-05 00:49:02 +0300

    funcitionality is about to end

--- script.js
+++ script.js
@@ -22,52 +22,138 @@ const game_object = ( function(){
    
 })() ;
 // check up
+const check_status_and_display = (function() {
+   let cur_ar = game_object.get_array();
 
-const player1 = game_object.object_creator( "player1","X");
-const player2 = game_object.object_creator( "player2","0");
-let current_player;
-let  game_over;
-let  counter;
+   function status_solver() {
+      if (cur_ar[0] === cur_ar[1] && cur_ar[1] === cur_ar[2] && cur_ar[0] !== "") {
+         if (cur_ar[0] === "X") {
+            console.log("player1 is a winner");
+         } else {
+            console.log("player2 is a winner");
+         }
+      } else if (cur_ar[3] === cur_ar[4] && cur_ar[4] === cur_ar[5] && cur_ar[3] !== "") {
+         if (cur_ar[3] === "X") {
+            console.log("player1 is a winner");
+         } else {
+            console.log("player2 is a winner");
+         }
+      } else if (cur_ar[6] === cur_ar[7] && cur_ar[7] === cur_ar[8] && cur_ar[6] !== "") {
+         if (cur_ar[6] === "X") {
+            console.log("player1 is a winner");
+         } else {
+            console.log("player2 is a winner");
+         }
+      } else if (cur_ar[0] === cur_ar[3] && cur_ar[3] === cur_ar[6] && cur_ar[0] !== "") {
+         if (cur_ar[0] === "X") {
+            console.log("player1 is a winner");
+         } else {
+            console.log("player2 is a winner");
+         }
+      } else if (cur_ar[1] === cur_ar[4] && cur_ar[4] === cur_ar[7] && cur_ar[1] !== "") {
+         if (cur_ar[1] === "X") {
+            console.log("player1 is a winner");
+         } else {
+            console.log("player2 is a winner");
+         }
+      } else if (cur_ar[2] === cur_ar[5] && cur_ar[5] === cur_ar[8] && cur_ar[2] !== "") {
+         if (cur_ar[2] === "X") {
+            console.log("player1 is a winner");
+         } else {
+            console.log("player2 is a winner");
+         }
+      } else if (cur_ar[0] === cur_ar[4] && cur_ar[4] === cur_ar[8] && cur_ar[0] !== "") {
+         if (cur_ar[0] === "X") {
+            console.log("player1 is a winner");
+         } else {
+            console.log("player2 is a winner");
+         }
+      } else if (cur_ar[6] === cur_ar[4] && cur_ar[4] === cur_ar[2] && cur_ar[6] !== "") {
+         if (cur_ar[6] === "X") {
+            console.log("player1 is a winner");
+         } else {
+            console.log("player2 is a winner");
+         }
+      } 
+   }
+
+   return {
+      status_solver
+   };
+})();
 
 
-function startup(){
-  current_player=player1
-  game_over=false
-  counter=0
- 
-}
-function game_flow(event) {
-   let button_value =event.target.value;
+
+
+
+
+const check= (function(document){
+   const player1 = game_object.object_creator( "player1","X");
+   const player2 = game_object.object_creator( "player2","0");
+   let current_player;
+   let  game_over;
+   let  counter;
    
    
-  
-     
-   if (game_over) {
-      return;
-   } else {
+   function startup(){
+     current_player=player1
+     game_over=false
+     counter=0
+    
+   }
+   function game_flow(event) {
+      let button_value =event.target.value;
       
-      if (counter === 9) {
-         game_over = true;
+      
+     
+     
+      if (game_over) {
+         return;
+      } else {
          counter++;
+         if (counter === 9) {
+            game_over = true;
+            console.log(`game is over and counter is reached ${game_over} `)
+            event.target.textContent= current_player.marker
+            event.target.disabled= true
+           
+         }
+         else{
+           
+          current_array=game_object. get_array()
+          if(current_array[button_value]==="")
+           current_array[button_value]=current_player.marker
+           event.target.textContent= current_player.marker
+           event.target.disabled= true
+       
+         check_status_and_display.status_solver()
+         }
+      }
+      if(current_player=== player1){
+         current_player =player2
       }
       else{
-       current_array=game_object. get_array()
-       if(current_array[button_value]==="")
-        current_array[button_value]=current_player.marker
-        event.target.textContent= current_player.marker
-        event.target.disabled= true
-      //  console.log(current_array)
+         current_player=player1
       }
+      
+      console.log(counter)
+      console.log(`current array is ${current_array}`)
    }
-}
+     
+   let buttons = document.querySelectorAll(".buttons");
+   buttons.forEach((element)=>{
+      element.addEventListener("click",game_flow)
+   }) 
+   
+   let btn_play=document.querySelector(".play");
+    btn_play.addEventListener("click" , startup)
 
-let buttons = document.querySelectorAll(".buttons");
-buttons.forEach((element)=>{
-   element.addEventListener("click",game_flow)
-}) 
+    return { player1,player2,current_player}
+  
+})(document)
 
-let btn_play=document.querySelector(".play");
- btn_play.addEventListener("click" , startup)
+
+
 
 
 // let current =game_object.get_array()
